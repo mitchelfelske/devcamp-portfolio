423df6f995f3e7b60b1d5301d97b97b936db0f75
mitchel.felske <mitchel.felske@ufsc.br>
2019-06-30 13:21:57 -0300

    Change default config and add index custom template for scaffold

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,22 @@ Bundler.require(*Rails.groups)
 
 module DevCampPortfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+		# Initialize configuration defaults for originally generated Rails version.
+		config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+		# Settings in config/environments/* take precedence over those specified here.
+		# Application configuration can go into files in config/initializers
+		# -- all .rb files in that directory are automatically loaded after loading
+		# the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+		# Don't generate system test files.
+		config.generators.system_tests = nil
+
+		config.generators do |g|
+			g.orm :active_record
+			g.template_engine :erb
+			g.stylesheets false
+			g.javascripts false
+		end
   end
 end
